fd6806b4986d0dc3708b8e3a85a1aca8c8096ab0
KanishkJagya1 <118714851+KanishkJagya1@users.noreply.github.com>
2024-06-14 01:26:21 +0530

    leetcode daily

---
+++ Easy Problems/Minimum_Number_of_Moves_to_Seat_Everyone.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+        int n = seats.size();
+        int m = students.size();
+        sort(seats.begin(),seats.end());
+        sort(students.begin(),students.end());
+        int result = 0;
+        for(int i = 0 ; i < seats.size() ; i++ ) {
+            result += abs(seats[i] - students[i]);
+        }
+        
+        return result;
+    }
+};
